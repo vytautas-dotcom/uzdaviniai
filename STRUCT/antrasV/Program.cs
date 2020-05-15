aa812385a7f801d5d85c74e70764c3886d2cdb52
Vytautas <tumas.vytautas@gmail.com>
2020-05-15 17:59:42 +0300

    STRUCT TRECIAS NEPABAIGTAS

---
+++ STRUCT/antrasV/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antrasV
+{
+    struct Koordinates
+    {
+        public int x;
+        public int y;
+        public void setKoordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void sudetiKoordinates()
+        {
+            Console.WriteLine($"{x + y}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Koordinates koordinates = new Koordinates();
+            koordinates.setKoordinates(5, 8);
+            koordinates.sudetiKoordinates();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ STRUCT/treciasV/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treciasV
+{
+    struct Studentas
+    {
+        static int count = 0;
+        string[] Vardas;
+        string[] Pavarde;
+        int[] Pazymys;
+
+        string vardas;
+        string pavarde;
+        int pazymys;
+        
+
+        public void setStudentas(string vardas, string pavarde, int pazymys)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.pazymys = pazymys;
+
+            saveStudentas(vardas, pavarde, pazymys, count);
+            count++;
+        }
+        public void saveStudentas(string a, string b, int c, int d)
+        {
+            this.Vardas[d] = a;
+            this.Pavarde[d] = b;
+            this.Pazymys[d] = c;
+        }
+        public void getStudentas(int studentoNr)
+        {
+            Console.WriteLine($"Vardas: {Vardas[studentoNr]}, Pavarde: {Pavarde[studentoNr]}, Pazymys: {Pazymys[studentoNr]}");  
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Studentas studentas = new Studentas();
+            studentas.setStudentas("Jonas", "Jonaitis", 10);
+            studentas.setStudentas("Petras", "Petraitis", 10);
+            studentas.setStudentas("Antanas", "Antanaitis", 10);
+
+            studentas.getStudentas(3);
+
+            Console.ReadKey();
+
+        }
+    }
+}
