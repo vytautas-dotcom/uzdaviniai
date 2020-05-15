f8b4442639330bcfed31f28bd84ca895977f811d
Vytautas <tumas.vytautas@gmail.com>
2020-05-15 16:13:40 +0300

    SWITCH PIRMAS

--- SWITCH/pirmas/Program.cs
+++ SWITCH/pirmas/Program.cs
@@ -10,6 +10,36 @@ namespace pirmas
     {
         static void Main(string[] args)
         {
+            gaukSpalva();
+
+            Console.ReadKey();
+        }
+        static void gaukSpalva()
+        {
+            Console.WriteLine("Iveskite varda ir Spalva (red, green arba blue)");
+            string vardas = Console.ReadLine();
+            string spalva = Console.ReadLine();
+
+            switch (spalva)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(vardas);
+                    Console.ResetColor();
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine(vardas);
+                    Console.ResetColor();
+                    break;
+                case "blue":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(vardas);
+                    Console.ResetColor();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
