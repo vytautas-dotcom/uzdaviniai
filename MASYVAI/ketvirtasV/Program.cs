113bba853c8ceef8d716bbcfa327e9d333709aa5
Vytautas <tumas.vytautas@gmail.com>
2020-05-17 13:13:35 +0300

    Pabaigti MASYVAI

--- MASYVAI/ketvirtasV/Program.cs
+++ MASYVAI/ketvirtasV/Program.cs
@@ -10,6 +10,51 @@ namespace ketvirtasV
     {
         static void Main(string[] args)
         {
+            Masyvas1();
+
+            Console.ReadKey();
+        }
+        static void Masyvas1()
+        {
+            Console.WriteLine("Masyvo dydi");
+            int nr = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[nr];
+            int[] brr = new int[nr];
+            Random rand = new Random();
+            int ctr;
+
+            for (int i = 0; i < nr; i++)
+            {
+                arr[i] = rand.Next(1, 11);
+                brr[i] = -1;
+            }
+
+            for (int i = 0; i < nr; i++)
+            {
+                ctr = 1;
+                for (int j = i + 1; j < nr; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        ctr++;
+                        brr[j] = 0;
+                    }
+                }
+
+                if (brr[i] != 0)
+                {
+                    brr[i] = ctr;
+                }
+            }
+            Console.Write("\nElementu pasikartojimo daznis: \n");
+            for (int l = 0; l < nr; l++)
+            {
+                if (brr[l] != 0)
+                {
+                    Console.Write("{0} elementas kartojasi {1} kartus\n", arr[l], brr[l]);
+                }
+            }
+
         }
     }
 }
---
+++ MASYVAI/penktasV/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penktasV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            int eilutes = arr.GetUpperBound(0) + 1;
+            int stulpeliai = arr.Length / eilutes;
+            int suma = 0;
+
+            for (int i = 0; i < eilutes; i++)
+            {
+                for (int j = 0; j < stulpeliai; j++)
+                {
+                    suma += arr[i, j];
+                }
+            }
+
+            Console.WriteLine(suma);
+
+            Console.ReadLine();
+        }
+    }
+}
