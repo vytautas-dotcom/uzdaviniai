28f22ec137b7f8c69fa942d6b1c42a3b40c02607
Andriejus <a.gorbunovas@gmail.com>
2020-05-17 10:48:46 +0300

    Pakoreguota fibonaci seka

--- NAMU_DARBAI/pirmasV/Program.cs
+++ NAMU_DARBAI/pirmasV/Program.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pirmasV
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Iveskite skaiciu, keliu Fibonacci sekos neriu suma nori rasti");
             Console.WriteLine(Fib(Int32.Parse(Console.ReadLine())));
 
             Console.ReadKey();
         }
-        static int Fib(int n)
+
+        private static int Fib(int n)
         {
-            if (n == 0 || n == 1)
+            if (n == 0)
+                return 0;
+            else if (n == 1)
                 return 1;
-            else
-                return Fib(n - 1) + Fib(n - 2);
+
+            return Fib(n - 1) + Fib(n - 2);
         }
     }
-    
 }
