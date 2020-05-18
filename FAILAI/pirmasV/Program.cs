90679639e85ea24a2f6d53c8f1bd512dd75f198d
Vytautas <tumas.vytautas@gmail.com>
2020-05-19 00:53:37 +0300

    Pirmas su failais

---
+++ FAILAI/pirmasV/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace pirmasV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reikia tik savo kompe konkrecia vieta nurodyt ir ten but sukurus txt
+            VardasPavarde();
+
+            Console.ReadKey();
+        }
+        static void VardasPavarde()
+        {
+            Console.WriteLine("Iveskite savo varda ir pavarde");
+            string vardas = Console.ReadLine();
+            string pavarde = Console.ReadLine();
+
+            string failoVieta = @"C:\Users\tumas\source\failas.txt";
+            string tekstasFailui = vardas + " " + pavarde;
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(failoVieta, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(tekstasFailui);
+                }
+
+                using (StreamWriter sw = new StreamWriter(failoVieta, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("kjfgukfxnhgd");
+                    sw.Write(1000000000);
+                }
+                Console.WriteLine("Pavyko!");
+                FileInfo fi = new FileInfo(failoVieta);
+                Console.WriteLine($"Failo dydis: {fi.Length}");
+                Console.WriteLine($"Failas yra: {failoVieta}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
