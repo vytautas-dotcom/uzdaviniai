97bf3456b383bde9fe7d42694935f4c823ec02fb
Vytautas <tumas.vytautas@gmail.com>
2020-05-18 14:34:24 +0300

    automobilis

---
+++ AUTOMOBILIS/pirmasV/Automobilis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pirmasV
+{
+    class Automobilis
+    {
+        private string pavadinimas;
+        private int rida;
+        private int kuras;
+        private readonly int maxTalpa = 50;
+
+        public void setDuomenys(string pavadinimas, int rida, int kuras)
+        {
+            this.pavadinimas = pavadinimas;
+            this.rida = rida;
+            this.kuras = kuras;
+        }
+        public void Vaziuoti()
+        {
+            rida+=10;
+            kuras--;
+        }
+
+        public void Pildyti()
+        {
+            Console.WriteLine("Kiek norit uzsipilt (Svarbu nevirsyti max talpos!)");
+            int litrai = Convert.ToInt32(Console.ReadLine());
+            if (litrai + kuras > maxTalpa)
+            {
+                Console.WriteLine($"Virsijate galima kieki {maxTalpa - kuras - litrai} litrais");
+            }
+        }
+    }
+}
---
+++ STATINEklaseKALKULIATORIUI/pirmasV/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pirmasV
+{
+    static class Calculator
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Substract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public static void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public static void Divide(int a, int b)
+        {
+            Console.WriteLine((double)(a / b));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Add(12, 6);
+            Calculator.Substract(12, 6);
+            Calculator.Multiply(12, 6);
+            Calculator.Divide(12, 6);
+
+            Console.ReadKey();
+        }
+    }
+}
